fc79a78f18ed91ed0142ee05541ff500bfae671e
Linus Lippert <linus.lippert@students.uni-mannheim.de>
2025-08-12 13:38:24 +0200

    changing get target

--- THz/getting_target.py
+++ THz/getting_target.py
@@ -15,4 +15,4 @@ for scan in os.listdir("D:/700GHz/mat_files/train/"):
         processed_data, max_val_abs = prdf.process_complex_data(complex_raw_data, int(parameters["NF"]), device=device)
         depth_layer = 700
         image_depth = torch.flipud(torch.pow(torch.abs(processed_data[depth_layer, ...]), 2)/max_val_abs).transpose(0, 1).detach().cpu().numpy()
-        skimage.io.imsave('THz/USAF_images/image_z_' + os.path.basename(filename_mat) + str(depth_layer) + '.tiff', image_depth, plugin="tifffile", check_contrast=False)
+        skimage.io.imsave('THz/USAF_images/tiff/image_z_' + os.path.basename(filename_mat).split('.')[0] + str(depth_layer) + '.tiff', image_depth, plugin="tifffile", check_contrast=False)
